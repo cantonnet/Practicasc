97337f8fb69dab32354c12913618086909c473fd
Alejandro Cantonnet <77518342+cantonnet@users.noreply.github.com>
2024-06-15 17:41:47 -0300

    Practica 43

--- Program.cs
+++ Program.cs
@@ -13,7 +13,21 @@ namespace Practicasc
             string nombre = "Alejandro";
             Console.WriteLine(nombre.Substring(4));
             Console.WriteLine(nombre);
+            Calcular();
             Console.Read();
+        }
+
+        public static void Calcular()
+        {
+            Console.WriteLine("Ingresar un numero");
+            string numero1 = Console.ReadLine();
+            Console.WriteLine("Ingresar segundo numero");
+            string numero2 = Console.ReadLine();
+
+            int num1 = int.Parse(numero1);
+            int num2 = int.Parse(numero2);
+            int resultado = num1 + num2;
+            Console.WriteLine("resultado de la suma de los numeros es: "+resultado);
         }
     }
 }
