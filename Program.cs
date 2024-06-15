df3af1b82baf482969402b77619fa73831c3c5c2
Alejandro Cantonnet <77518342+cantonnet@users.noreply.github.com>
2024-06-15 17:49:52 -0300

    practica 44

--- Program.cs
+++ Program.cs
@@ -23,11 +23,20 @@ namespace Practicasc
             string numero1 = Console.ReadLine();
             Console.WriteLine("Ingresar segundo numero");
             string numero2 = Console.ReadLine();
+            int num1 = 0;
+            int num2 = 0;
+            try
+            {
+                num1 = int.Parse(numero1);
 
-            int num1 = int.Parse(numero1);
-            int num2 = int.Parse(numero2);
+                num2 = int.Parse(numero2);
+            }
+            catch 
+            {
+                Console.WriteLine("Lo ingresado no es un numero");
+            }
             int resultado = num1 + num2;
-            Console.WriteLine("resultado de la suma de los numeros es: "+resultado);
+            Console.WriteLine("resultado de la suma de los numeros {0} y {1} es: {2}",num1,num2,resultado);
         }
     }
 }
